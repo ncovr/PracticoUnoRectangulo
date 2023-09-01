d8c68ecb114351a24d17100f3d69d3888eb7098d
Nicolás Verdugo <nicolas.verdugo2301@alumnos.ubiobio.cl>
2023-08-31 22:09:56 -0400

    hola

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Hola");
 
     }
 }
