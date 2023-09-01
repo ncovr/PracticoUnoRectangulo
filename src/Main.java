cf485a1084a35f20c972b12441ef16e99a875953
Nicolás Verdugo <nicolas.verdugo2301@alumnos.ubiobio.cl>
2023-08-31 23:09:34 -0400

    Firt object

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("wtf k pasa");
+
+        int largo = 4;
+        int ancho = 2;
+        // Creando mi primer objeto
+        Rectangulo primerRectangulo = new Rectangulo(ancho, largo);
+        System.out.println(primerRectangulo.area());
+
     }
 }
---
+++ src/Rectangulo.java
@@ -0,0 +1,26 @@
+public class Rectangulo {
+    private int ancho;
+    private int largo;
+
+    public Rectangulo(int ancho, int largo){
+        this.ancho = ancho;
+        this.largo = largo;
+    }
+
+    public void setAncho(int a){
+        ancho = a;
+    }
+
+    public void setLargo(int l){
+        largo = l;
+    }
+
+    public int area(){
+        return (largo*ancho);
+    }
+
+    public int perimetro(){
+        return ((largo*2)+(ancho*2));
+    }
+
+}
